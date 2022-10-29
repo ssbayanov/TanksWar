d7970cf5bccad282a8b65c378ebdd745da550f1f
888ni888 <nick280210@gmail.com>
2022-10-29 12:18:50 +0700

    meny_nodocontsa

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,6 +1,35 @@
 extends Node
 
-
+var tank_bodies = {
+	'standart': {
+		'hp' : 100,
+		'places' : 1,
+		'speed' : 700,
+		'texture' : "res://Images/tankBody_red_outline.png",
+		'max_type_barrel' : 2,
+	},
+	'better_tank': {
+		'hp' : 200,
+		'places' : 1,
+		'speed' : 650,
+		'texture' :"res://Images/tankBody_darkLarge_outline.png",
+		'max_type_barrel' : 4 ,
+		},
+	'big_tank' : {
+		'hp' : 300,
+		'places' : 2,
+		'speed' : 500,
+		'texture' :"res://Images/tankBody_bigRed_outline.png",
+		'max_type_barrel' : 6 ,
+		},
+	'best_tank' : {
+		'hp' : 500,
+		'places' : 2,
+		'speed' : 400,
+		'texture' :"res://Images/tankBody_huge_outline.png",
+		'max_type_barrel' : 10 ,
+		},
+}
 
 func _ready():
 	randomize()
