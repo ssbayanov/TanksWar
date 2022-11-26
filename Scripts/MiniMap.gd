a5396e3d0cae892e2a26291c103dde31ed6df366
kwertyW <106729573+kwertyW@users.noreply.github.com>
2022-11-26 13:12:10 +0700

    MiniMap made

--- Scripts/MiniMap.gd
+++ Scripts/MiniMap.gd
@@ -1,117 +1,36 @@
 extends VBoxContainer
 
-export var zoom = 1 setget set_zoom
+export var zoom = 1 
+var player
+var marker_list = []
 
+onready var enemy_list = get_tree().get_nodes_in_group("enemy")
+onready var viewport = $Control/ViewportContainer/Viewport
+onready var camera = $Control/ViewportContainer/Viewport/Camera2D
+func _ready(): for e in enemy_list: add_marker(e)
 
-onready var grid = self
-onready var player_marker = $PlayerMarker
-
-
-
-var markers = []
-var grid_scale
-var map_objects
-var player = player_marker
-var map = null
-
-func _ready():
-	map_objects = get_tree().get_nodes_in_group("minimap_objects")
-	for object in map_objects:
-		if object.name == "player": 
-			player = object # нашли игрока и запомнили
-			
+# функции подгрусски данных
 func set_map(m):
-	map = m
-	$ViewportContainer/Viewport.add_child(map.get_node("ground").duplicate())
-	$ViewportContainer/Viewport.add_child(map.get_node("roads").duplicate())
+	viewport.add_child(m.get_node("ground").duplicate());
+	viewport.add_child(m.get_node("roads").duplicate());
+func set_player(p): player = p;
 
+
+func add_marker(obj): # добовляем новый маркер
+	var new_marker = load("res://Scence/marker.tscn").instance()
+	new_marker.obj = obj
+	viewport.add_child(new_marker)
 	
 func _process(delta):
-	$ViewportContainer/Viewport/Camera2D.position = player.position
-	
-	map_objects = get_tree().get_nodes_in_group("minimap_objects") # получаем массив обьектов кторые должны быть на карте
-	player_marker.position = self.rect_size / 2 # Ставим игрока в центре
-	grid_scale = grid.rect_size / (get_viewport_rect().size * zoom) # задаём маштаб
-	$zoom_see.text = "ZOOM -" + str(zoom)
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	while len(map_objects) != len(markers):
-		if len(map_objects)> len(markers):
-			get_marker()
-		elif len(map_objects) < len(markers):
-			markers[len(markers) - 1].queue_free()
-			markers.pop_back()
-			
-	for indecs in range(len(map_objects)):
-		var object = map_objects[indecs]
-		var marker = markers[indecs-1]
-		if object.name == "player": marker.hide(); player_marker.rotation_degrees = object.rotation_degrees + 180; continue
-		
-		object.show()
-		marker.position = set_pos_marker(pos(object.position, player.position), marker)
+	if player == null: return; # Проверка на подгруску персонажа.
+	camera.position = player.position # Задаю позицию камеры в Viewport.
+	camera.zoom = Vector2(zoom * 3, zoom * 3) #Задаю размер камеры
+	$Control/PlayerMarker.rect_rotation = player.rotation_degrees + 180 # Поворачиваю с соотвецтвием с игроком.
+	$zoom_see.text = "ZOOM -" + str(zoom) # Показываю размер зумма.
 
-		
-		"""
-		Сделать ограничение по минимальному
-		Сделать показ фона с tilemap (карты)
-		"""
-		
-		
-		
-		
-		
-	
-	
-	
-
-func get_marker():
-	var marker = Sprite.new()
-	marker.texture = load("res://Images/barrelBlack_top.png")
-	add_child(marker)
-	markers.append(marker)
-	
-
-func set_zoom(value):
-	zoom = clamp(value, 0.5, 5)
-	grid_scale = grid.rect_size / (get_viewport_rect().size * zoom)
-	
-	
-func _input(event):
+func _input(event): # Функция для изменения покозтиля размер карты от пользователя.
 	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_WHEEL_UP:
-			self.zoom -= 0.1
-		if event.button_index == BUTTON_WHEEL_DOWN:
-			self.zoom += 0.1
-
-func pos(object_pos, player_pos): return ((object_pos - player_pos) * grid_scale + self.rect_size/2) - Vector2(20, 20)
-
-
-func set_pos_marker(pos, marker):	
-	#marker.scale = Vector2(((marker.position / $MarginContainer.rect_size).x + (marker.position / $MarginContainer.rect_size).y) / 2 + 0.4, ((marker.position / $MarginContainer.rect_size).x + (marker.position / $MarginContainer.rect_size).y) / 2 + 0.4)
-	if pos.x > (self.rect_size).x / 1.2:
-		pos.x = (self.rect_size).x / 1.2
-	if pos.x < 0:
-		pos.x = 0
-	if pos.y > (self.rect_size).y / 1.2:
-		pos.y = (self.rect_size).y / 1.2
-	if pos.y < 0:
-		pos.y = 0
-		
-#	if pos.x > (self.rect_size).x * 2:
-#		marker.hide()
-#	if pos.x < -(self.rect_size).y * 2:
-#		marker.hide()
-#	if pos.y > (self.rect_size).y * 2:
-#		marker.hide()
-#	if pos.y < -(self.rect_size).y * 2:
-#		marker.hide()
-		
-	return pos
-
+		if event.button_index == BUTTON_WHEEL_UP: zoom -= 0.1;
+		if event.button_index == BUTTON_WHEEL_DOWN: zoom += 0.1;
+		if zoom > 6: zoom = 6
+		if zoom < 0.5: zoom = 0.5
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,7 +4,7 @@ extends Node2D
 func _ready():
 	get_data_for_player()
 	$CanvasLayer/MiniMap.set_map($map)
-
+	$CanvasLayer/MiniMap.set_player($player)
 
 
 func get_data_for_player():
@@ -12,3 +12,4 @@ func get_data_for_player():
 	for i in enemy:
 		print('it is print enemy', enemy)
 		i.player_tank = $player
+
