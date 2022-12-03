3546b5e730c68361212ec097cca6556b1409c1fe
888ni888 <nick280210@gmail.com>
2022-12-03 11:28:57 +0700

    Add dictionary of tanks and barrels

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,41 +1,72 @@
 extends Node
 
-var tank_bodies = {
-	'standart': {
-		'hp' : 100,
-		'places' : 1,
-		'speed' : 700,
-		'texture' : "res://Images/tankBody_red_outline.png",
-		'max_type_barrel' : 2,
-	},
-	'better_tank': {
-		'hp' : 200,
-		'places' : 1,
-		'speed' : 650,
-		'texture' :"res://Images/tankBody_darkLarge_outline.png",
-		'max_type_barrel' : 4 ,
-		},
-	'big_tank' : {
-		'hp' : 300,
-		'places' : 2,
-		'speed' : 500,
-		'texture' :"res://Images/tankBody_bigRed_outline.png",
-		'max_type_barrel' : 6 ,
-		},
-	'best_tank' : {
-		'hp' : 500,
-		'places' : 2,
-		'speed' : 400,
-		'texture' :"res://Images/tankBody_huge_outline.png",
-		'max_type_barrel' : 10 ,
-		},
-}
+var tanks = {
+	'standart':{
+		'sprite': "res://Images/tankBody_dark.png",
+		'barrel_count': 1,
+		'hp': 100,
+		'speed': 1000,
+		'max_barrel_type': 1,
+		'barrel_pos': [
+			Vector2(775, 430),
+   ]
+  },
+ 
+ 'big_tank': {
+  'sprite': "res://Images/tankBody_darkLarge_outline.png",
+  'barrel_count': 2,
+  'hp': 150,
+  'speed': 700,
+  'max_barrel_type': 2,
+  'barrel_pos': [
+   Vector2(827, 415),
+   Vector2(847, 415),
+   ]
+  },
+ 'the_biggest_tank': {
+  'sprite': "res://Images/tankBody_huge_outline.png",
+  'barrel_count': 4,
+  'hp': 250,
+  'speed': 450,
+  'max_barrel_type': 3,
+  'barrel_pos': [
+   Vector2(-7, -9),
+   Vector2(7, -9),
+   Vector2(-7, 28),
+   Vector2(7, 28),
+   ]
+  }
+ }
 
-func _ready():
-	randomize()
+var barrel = {
+ 'barrel_low':{
+  'cost': 500,
+  'sprite': "res://Images/tankDark_barrel2_outline.png",
+  'barrel_type': 1,
+  'damage': 50,
+  'speed': 50,
+  'cool_down': 1,
+  },
+ 'barrel_middle':{
+  'cost': 1000,
+  'sprite': "res://Images/tankDark_barrel3_outline.png",
+  'barrel_type': 2,
+  'damage': 20,
+  'speed': 40,
+  'cool_down': 0.5,
+  },
+ 'barrel_high':{
+  'cost': 1500,
+  'sprite': "res://Images/tankDark_barrel1_outline.png",
+  'barrel_type': 3,
+  'damage': 15,
+  'speed': 25,
+  'cool_down': 0.2,
+  }
+ }
+var keys_tanks = tanks.keys()
 
 func rand_rangei(start, stop):
 	if start == stop:
 		return start
 	return randi() % int((stop - start)) + int(start)
-
