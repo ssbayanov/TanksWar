7889ac8339ac0c5763dfd0054a75ada536d26533
ssbayanov <kwertyXS@gmail.com>
2023-04-08 10:15:41 +0700

    spawn enemys

--- Objects/NPC/turel.gd
+++ Objects/NPC/turel.gd
@@ -1,10 +1,9 @@
 extends KinematicBody2D
 
-
+var minimap_icon = "Mob"
 var mindistance = 100
 var maxdistance = 500
 var player_tank 
-
 var max_speed = 500 #максимальная скорость танка
 var acc = 100 #замедление танка (accsiliration)
 var dec = 300 #ускорение танка
@@ -19,45 +18,40 @@ var track_step = 10
 
 var barrel = true
 
+var time_in_map = 0 # нужно
+var ride_accuracy = 100 # нужно
+var max_destance_error = 1000
 
-onready var trackRes = load("res://Objects/Track/track.tscn")
+var timer = 0
+onready var trackRes = g.track# нужно
 
 func _ready():
-	bullet = load("res://Bullet/bullet1.tscn").instance()
+	bullet = g.bullets[1].instance()
 	change_hp(0)
-	
-	
-	
-	
-	
-	
+
+
 func _process(delta):
 	if not player_tank:
 		return
-	_shoot_delayer_process(delta)
+	time_in_map += delta
+	#_shoot_delayer_process(delta)
 
 
-
-
+var data_for_forward = [0, 0]
 
 func _physics_process(delta):
 	if not player_tank:
 		return
-	var distance = (player_tank.position - position).rotated(PI/2)
-	
-	if distance.length() < maxdistance and distance.length() > mindistance: 
-		if abs(c_speed.y) < max_speed: #меняем скорость на ускорение * время смены кадров
-			c_speed.y -= acc * delta 
-	else:
-		if abs(c_speed.y) >= 0:
-			if abs(c_speed.y) < 10:
-				c_speed.y = 0
-		else:
-			c_speed.y -= (dec*delta) * c_speed.y / abs(c_speed.y)
-	global_rotation = lerp_angle(global_rotation,distance.angle(),delta)
-	c_speed = move_and_slide(c_speed.rotated(rotation)).rotated(-rotation)
-	c_speed.x = 0
+	if timer >= 5:
+		data_for_forward = inaction()
+#		print(data_for_forward)
+		timer = 0
+		
+	global_rotation = lerp_angle(global_rotation,data_for_forward[0],delta * 5)
+	#c_speed = move_and_slide(c_speed.rotated(rotation)).rotated(-rotation)
+	#c_speed.x = 0
 
+	timer += delta
 	len_track += c_speed.length() * delta
 	if len_track >= track_step:
 		len_track = 0
@@ -84,10 +78,13 @@ func change_hp(amount):
 	hp +=amount
 	if hp <=0:
 		hp = 0
+		#signal("remove_turrel")
+		remove_from_group("minimap_objects")
 		boom()
 	if hp > 100:
 		hp = 100
 	$Node2D/hpbar.set_value(hp)
+	
 	
 
 
@@ -112,7 +109,7 @@ func _shoot_delayer_process(delta):
 	
 	
 func _shoot():
-	if shoot_delayer >= bullet.shoot_delay and player_tank.global_position.length() < 200:
+	if shoot_delayer >= bullet.shoot_delay:
 		var newbullet = bullet.duplicate()
 		get_parent().add_child(newbullet)
 		newbullet.shoot_delay = 0.2
@@ -154,3 +151,22 @@ func _on_shooot_animation_finished():
 func _on_shooot2_animation_finished():
 	$shooot2.set_frame(0)
 	$shooot2.hide()
+
+
+
+func inaction():
+	var pos = global_position
+	var where_pos = player_tank.position
+	var distance = where_pos - pos
+	var evclidion_distance = pow(pow(distance.x, 2) + pow(distance.y ,2), 0.5)
+	var angel = pos.angle_to(where_pos)
+	
+	var rot_error = g.rand_rangei((ride_accuracy - 100), (100 - ride_accuracy)) / 100.0 * PI 
+	var distance_error = g.rand_rangei((ride_accuracy - 100), (100 - ride_accuracy)) / 100.0 * max_destance_error# / 100 
+
+#	print("Угол -", rad2deg(angel))
+#	print(rad2deg(rot_error))
+#	print(distance_error)
+
+	return [(rot_error + angel), distance_error]
+	
--- Stage/stage.gd
+++ Stage/stage.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 onready var map = $map
 
+var tree_positions = []
+
 func _ready():
 	rand_seed(map.map_seed)
 	get_data_for_player()
@@ -28,7 +30,9 @@ func rand_rangei(start, stop):
 func generate_objects():
 	generate_tree()
 	generate_mines()
-
+	generate_enemys()
+	generate_boxes()
+	
 func generate_tree(cofficente_abaut:int = -1, cofficente_to:int = -0.58):
 	var noise = map.get_noise(map.size, 50)
 	var tree = load("res://Objects/Tree/treegreen.tscn")
@@ -43,8 +47,16 @@ func generate_tree(cofficente_abaut:int = -1, cofficente_to:int = -0.58):
 				continue
 			var object = tree.instance()
 			$objects/tree.add_child(object)
-			object.global_position = Vector2((x * 64) + randi() % 32, (y * 64) + randi() % 32)
-
+			var xr = randi()
+			var yr = randi()
+			object.global_position = Vector2((x * 64) + xr % 32, (y * 64) + yr % 32)
+			tree_positions.append(Vector2((x * 64) + xr % 32, (y * 64) + yr % 32))
+			
+func check_pos(x, y): 
+	for pos in tree_positions: 
+		if stepify(pos.x, 1) - x < 1 and stepify(pos.y, 1) - y < 1: 
+			return false
+	return true
 
 func generate_mines():
 	var caunt = rand_rangei(5, 10)
@@ -58,3 +70,35 @@ func generate_mines():
 		new_mine.type_of_explosion = rand_rangei(0, 3)
 		$objects/Mines.add_child(new_mine)
 		new_mine.global_position = Vector2((x * 64) + 32, (y * 64) + 32)
+
+func generate_enemys():
+	var objects = [load("res://Objects/NPC/tanknpc.tscn"), load("res://Objects/NPC/turellnpc.tscn")]
+	for object in objects:
+		var caunt = rand_rangei(3, 7)
+		rand_seed(map.map_seed)
+		for i in range(caunt):
+			var new_mine = object.instance()
+			var x = rand_rangei(0, map.size.x)
+			var y = rand_rangei(0, map.size.y)
+			while not check_pos(x, y):
+				x = rand_rangei(0, map.size.x)
+				y = rand_rangei(0, map.size.y)
+			$objects/Mines.add_child(new_mine)
+			new_mine.global_position = Vector2((x * 64) + 32, (y * 64) + 32)
+	get_data_for_player()
+
+func generate_boxes():
+	var objects = [load("res://Barrel/Barel.tscn"), load("res://Objects/Box/box.tscn"), 
+	load("res://Objects/Hedhehog/barecata.tscn"), load("res://Objects/Hedhehog/Hedgehog.tscn")]
+	
+	
+	for object in objects:
+		var caunt = rand_rangei(5, 10)
+		rand_seed(map.map_seed)
+		
+		for i in range(caunt):
+			var new_mine = object.instance()
+			var x = rand_rangei(0, map.size.x)
+			var y = rand_rangei(0, map.size.y)
+			$objects/Mines.add_child(new_mine)
+			new_mine.global_position = Vector2((x * 64) + 32, (y * 64) + 32)
